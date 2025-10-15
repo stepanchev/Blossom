b148bbce8169b1931ed2a052be7aa3d3d12849d0
blessing <130278891+stepanchev@users.noreply.github.com>
2025-10-15 22:22:30 +0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Код для пузырьков
   const container = document.getElementById("bubbles-container");
   const bubbleCount = 15;
 
@@ -6,18 +7,44 @@ document.addEventListener("DOMContentLoaded", function () {
     const bubble = document.createElement("div");
     bubble.classList.add("bubble");
 
-    // Случайные размеры
     const size = Math.random() * 30 + 10;
     bubble.style.width = `${size}px`;
     bubble.style.height = `${size}px`;
 
-    // Случайное начальное положение
     bubble.style.left = `${Math.random() * 100}%`;
     bubble.style.top = `${Math.random() * 100}%`;
 
-    // Случайная задержка для анимации
     bubble.style.animationDelay = `${Math.random() * 6}s`;
 
     container.appendChild(bubble);
   }
+
+  const buttons = document.querySelectorAll(".nomera_pogination");
+  buttons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      const targetId = this.dataset.target;
+
+      const targetContainer = document.getElementById(targetId);
+      if (!targetContainer) return;
+
+      document
+        .querySelectorAll(".card-section-wrapper")
+        .forEach((container) => {
+          container.classList.remove("activ");
+        });
+
+      targetContainer.classList.add("activ");
+
+      buttons.forEach((btn) => btn.classList.remove("active"));
+
+      this.classList.add("active");
+
+      targetContainer.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  });
 });
